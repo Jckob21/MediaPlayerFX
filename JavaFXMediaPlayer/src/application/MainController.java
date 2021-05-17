2ef543681cd820d56de06c085d6cc054638dbd64
Jacob <jckob21@gmail.com>
2021-05-17 23:37:40 +0200

    Added buttons
    Added functionality of play/pause, -10sec, +10sec, speedUp, slowDown and beggining button. The displayMessage to be fixed, timer not working correctly (should wait for a second and delete the message). Also there is still possibility of binding a property to Duration's object current time.

--- JavaFXMediaPlayer/src/application/MainController.java
+++ JavaFXMediaPlayer/src/application/MainController.java
@@ -3,21 +3,34 @@ package application;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+//NOT working with javaFX correctly
+/*import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;*/
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+//import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 public class MainController implements Initializable {
+	public static boolean isPlaying = false;	
+	
 	//Media player section
 	@FXML
 	private MediaView mvMain;
 	private MediaPlayer mediaPlayerMain;
 	private Media media;
+	
+	@FXML
+	private Label messageDisplayer;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -35,7 +48,69 @@ public class MainController implements Initializable {
 		
 		// Playing the video once loaded
 		mediaPlayerMain.setAutoPlay(true);
+		isPlaying = true;
+		
+		// bind to duration milliseconds to show the time on the label?
+		//DoubleProperty timeMili = new SimpleDoubleProperty(mediaPlayerMain.getCurrentTime().toMillis());
+		//timeMili.bind(mediaPlayerMain.getCurrentTime().toMillis());
 	}
 	
+	public void beginning(ActionEvent event)
+	{
+		mediaPlayerMain.setRate(1);
+		mediaPlayerMain.seek(mediaPlayerMain.getStartTime());
+	}
+	
+	public void slowDown(ActionEvent event)
+	{
+		mediaPlayerMain.setRate(mediaPlayerMain.getRate() * .9);
+		displayMessage("Speed ratio: " + Math.round(mediaPlayerMain.getRate() * 100) / 100.0);
+	}
+	
+	public void speedUp(ActionEvent event)
+	{
+		mediaPlayerMain.setRate(mediaPlayerMain.getRate() * 1.1);
+		displayMessage("Speed ratio: " + Math.round(mediaPlayerMain.getRate() * 100) / 100.0);
+	}
+	
+	public void minus10sec(ActionEvent event)
+	{
+		mediaPlayerMain.seek(mediaPlayerMain.getCurrentTime().subtract(new Duration(10000)));
+	}
+	
+	public void plus10sec(ActionEvent event)
+	{
+		mediaPlayerMain.seek(mediaPlayerMain.getCurrentTime().add(new Duration(10000)));
+	}
+	
+	public void playPause(ActionEvent event)
+	{
+		if(isPlaying)
+		{
+			mediaPlayerMain.pause();
+		} else
+		{
+			mediaPlayerMain.play();
+		}
+		isPlaying = !isPlaying;
+	}
+	
+	
+	//not working?
+	private void displayMessage(String message)
+	{
+		System.out.println("called!");
+		messageDisplayer.setText(message);
+
+		// NOT working with javaFX correctly
+		/*final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+		executorService.scheduleWithFixedDelay(new Runnable() {
+			@Override
+			public void run() {
+				messageDisplayer.setText("");
+			}
+		}, 0, 1, TimeUnit.SECONDS);*/
+		
+	}
 
 }
