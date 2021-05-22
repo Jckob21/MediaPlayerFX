0667f39ba58c28c9b6a02c56861f1908bbdace03
Jacob <jckob21@gmail.com>
2021-05-22 15:48:42 +0200

    Added displaying time on label, help button action
    Changed lambda expression to normal ChangeListener and added setText with the currentTime / totalTime passed as an argument. Help button action now opens browser with github/project_link.

--- JavaFXMediaPlayer/src/application/MainController.java
+++ JavaFXMediaPlayer/src/application/MainController.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 //NOT working with javaFX correctly
@@ -28,158 +31,168 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.util.Duration;
 
 public class MainController implements Initializable {
-	public static boolean isPlaying = false;	
-	
-	//Media player section
+	public static boolean isPlaying = false;
+
+	// Media player section
 	@FXML
 	private MediaView mvMain;
 	private MediaPlayer mediaPlayerMain;
 	private Media media;
-	
+
 	@FXML
 	private Label messageDisplayer;
 	@FXML
 	private Slider volumeSlider;
 	@FXML
 	private Slider timeSlider;
-	
-	
+	@FXML
+	private Label timeDisplayer;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// Openning the example openning file
 		String filePath = new File("src/resources/testVideo.mp4").getAbsolutePath();
-		loadClip (new File(filePath));// delete String and loadClip lines and replace with the one below
-		//openClip(new ActionEvent());
-		
-		bindMVSizeToWindow ();		
-		
+		loadClip(new File(filePath));// delete String and loadClip lines and replace with the one below
+		// openClip(new ActionEvent());
+
+		bindMVSizeToWindow();
+
 		// Playing the video once loaded
 		mediaPlayerMain.setAutoPlay(true);
 		isPlaying = true;
-		
 
-		
-		//set volumeSlider value to the default volume and bind the slider value to media player volume.
+		// set volumeSlider value to the default volume and bind the slider value to
+		// media player volume.
 		volumeSlider.setValue(mediaPlayerMain.getVolume() * 100);
 		volumeSlider.valueProperty().addListener(new InvalidationListener() {
 
 			@Override
 			public void invalidated(Observable arg0) {
 				mediaPlayerMain.setVolume(volumeSlider.getValue() / 100.0);
-		}});
-		
+			}
+		});
+
+		// initializing time Slider
 		if (mediaPlayerMain.getStatus() == MediaPlayer.Status.UNKNOWN) {
-	        mediaPlayerMain.statusProperty().addListener((obs, oldStatus, newStatus) -> {
-	            if (newStatus == MediaPlayer.Status.READY) {
-	                initializeSlider();
-	            } 
-	        });
-	    } else {
-	        initializeSlider();
-	    }
-		
+			mediaPlayerMain.statusProperty().addListener((obs, oldStatus, newStatus) -> {
+				if (newStatus == MediaPlayer.Status.READY) {
+					initializeSlider();
+				}
+			});
+		} else {
+			initializeSlider();
+		}
+
 	}
-	
+
 	/**
 	 * Sets media player to start time of the file and resets speed rate to 1.
+	 * 
 	 * @param event
 	 */
-	public void beginning(ActionEvent event)
-	{
+	public void beginning(ActionEvent event) {
 		mediaPlayerMain.setRate(1);
 		mediaPlayerMain.seek(mediaPlayerMain.getStartTime());
 	}
-	
+
 	/**
 	 * Sets rate to 10% decreased value and displays appropriate message.
+	 * 
 	 * @param event
 	 */
-	public void slowDown(ActionEvent event)
-	{
+	public void slowDown(ActionEvent event) {
 		mediaPlayerMain.setRate(mediaPlayerMain.getRate() * .9);
 		displayMessage("Speed ratio: " + Math.round(mediaPlayerMain.getRate() * 100) / 100.0);
 	}
-	
+
 	/**
 	 * Sets rate to 10% increased value and displays appropriate message.
+	 * 
 	 * @param event
 	 */
-	public void speedUp(ActionEvent event)
-	{
+	public void speedUp(ActionEvent event) {
 		mediaPlayerMain.setRate(mediaPlayerMain.getRate() * 1.1);
 		displayMessage("Speed ratio: " + Math.round(mediaPlayerMain.getRate() * 100) / 100.0);
 	}
-	
+
 	/**
 	 * Subtracts ten seconds from current duration and sets mediaPlayer to it.
+	 * 
 	 * @param event
 	 */
-	public void minus10sec(ActionEvent event)
-	{
+	public void minus10sec(ActionEvent event) {
 		mediaPlayerMain.seek(mediaPlayerMain.getCurrentTime().subtract(new Duration(10000)));
 	}
-	
+
 	/**
 	 * Adds ten seconds to current duration and sets mediaPlayer to it.
+	 * 
 	 * @param event
 	 */
-	public void plus10sec(ActionEvent event)
-	{
+	public void plus10sec(ActionEvent event) {
 		mediaPlayerMain.seek(mediaPlayerMain.getCurrentTime().add(new Duration(10000)));
 	}
-	
+
 	/**
-	 * Handles playPause button. Uses isPlayed global variable to define if the video is playing or not
+	 * Handles playPause button. Uses isPlayed global variable to define if the
+	 * video is playing or not
+	 * 
 	 * @param event
 	 */
-	public void playPause(ActionEvent event)
-	{
-		if(isPlaying)
-		{
+	public void playPause(ActionEvent event) {
+		if (isPlaying) {
 			mediaPlayerMain.pause();
-		} else
-		{
+		} else {
 			mediaPlayerMain.play();
 		}
 		isPlaying = !isPlaying;
 	}
-	
-	
-	//TODO hide message after some time period 
-	private void displayMessage(String message)
-	{
+
+	// TODO hide message after some time period
+	private void displayMessage(String message) {
 		System.out.println("called!");
 		messageDisplayer.setText(message);
-		
+
 	}
-	
+
 	/**
-	 * Handles button "open new file": Opens filechooser, and if the file is valid opens it in the mediaview.
+	 * Handles button "open new file": Opens filechooser, and if the file is valid
+	 * opens it in the mediaview.
+	 * 
 	 * @param event
 	 */
-	public void openClip(ActionEvent event)
-	{
+	public void openClip(ActionEvent event) {
 		FileChooser fc = new FileChooser();
 		fc.setTitle("Select playable file");
 		fc.getExtensionFilters().add(new ExtensionFilter("Movie files", "*.mp4", "*.avi", "*.mov", "*.gif"));
 		File selectedFile = fc.showOpenDialog(null);
-		if(selectedFile != null)
-		{
+		if (selectedFile != null) {
 			loadClip(selectedFile);
 		}
 	}
-	
+
+	public void openHelpWebsite(ActionEvent event) {
+		try {
+			Desktop.getDesktop().browse(new URL("https://github.com/Jckob21/MediaPlayerFX").toURI());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	/**
 	 * Loads a new file to a MediaPlayer and passes it to mediaview.
+	 * 
 	 * @param fileToLoad file to load to the mediaView
 	 */
-	private void loadClip (File fileToLoad)
-	{
+	private void loadClip(File fileToLoad) {
 		media = new Media(fileToLoad.toURI().toString());
 		mediaPlayerMain = new MediaPlayer(media);
 		mvMain.setMediaPlayer(mediaPlayerMain);
 	}
-	
+
 	/**
 	 * Binds MediaView size to the window size.
 	 */
@@ -193,21 +206,18 @@ public class MainController implements Initializable {
 
 	private void initializeSlider() {
 	    timeSlider.setMax(mediaPlayerMain.getTotalDuration().toSeconds());
-	    mediaPlayerMain.currentTimeProperty().addListener((obs, oldTime, newTime) -> 
-	        timeSlider.setValue(newTime.toSeconds()));
-	}
-	
-	////////////////////THIS IS WORKING REGARDING TIME
-	//TODO If put in initialize method the duration is NAN so it is called to early, fix it!
-	/*timeSlider.setMax(mediaPlayerMain.getTotalDuration().toSeconds());
-	System.out.println("Max value: " + mediaPlayerMain.getTotalDuration().toSeconds());
-	mediaPlayerMain.currentTimeProperty().addListener(new ChangeListener() {
+	    mediaPlayerMain.currentTimeProperty().addListener(new ChangeListener() {
 
-		@Override
-		public void changed(ObservableValue o, Object oldVal, Object newVal) {
-			timeSlider.setValue(mediaPlayerMain.getCurrentTime().toSeconds());
-		}
-		
-	});*/
+			@Override
+			public void changed(ObservableValue arg0, Object arg1, Object arg2) {
+				// TODO Auto-generated method stub
+				timeSlider.setValue(mediaPlayerMain.getCurrentTime().toSeconds());
+				timeDisplayer.setText(mediaPlayerMain.getCurrentTime().toSeconds() + "/" + mediaPlayerMain.getTotalDuration().toSeconds());
+			}
+	    	
+	    	
+	    }
+	    ); 
+	}
 
 }
